69a8805250c3b32e1450bf5168f16a57bc510aa5
Ken Barlow <kenbrlow@gmail.com>
2021-03-01 17:37:20 -0700

    Moved counter to global scope to access same instance across functions. Also got minus and plus buttons working.

--- js/challenge.js
+++ js/challenge.js
@@ -2,17 +2,58 @@ document.addEventListener("DOMContentLoaded", () => {
     startNewPageProcess();
 });
 
+let counterElement = document.getElementById("counter");
+let currentCounterValue = parseInt(counterElement.innerText);
 
 function startNewPageProcess() {
     startCounter();
+    setupButtonMinus();
+    setupButtonPlus();
+    setupButtonHeart();
+    setupButtonPause();
 }
 
 function startCounter() {
-    let counterElement = document.getElementById("counter");
-    let currentCounterValue = parseInt(counterElement.innerText);
-    console.log({currentCounterValue})
     setInterval(function () {
         currentCounterValue = currentCounterValue + 1;
         counterElement.innerText = currentCounterValue;
     }, 1000);
+}
+
+function setupButtonMinus() {
+    let selectedElement = document.getElementById("minus");
+    selectedElement.addEventListener("click", function() {
+        runMinusButtonClicked();
+    });
+}
+
+function runMinusButtonClicked() {
+    currentCounterValue = currentCounterValue - 1;
+    counterElement.innerText = currentCounterValue;
+}
+
+function setupButtonPlus() {
+    let selectedElement = document.getElementById("plus");
+    selectedElement.addEventListener("click", function() {
+        runPlusButtonClicked();
+    });
+}
+
+function runPlusButtonClicked() {
+    currentCounterValue = currentCounterValue + 1;
+    counterElement.innerText = currentCounterValue;
+}
+
+function setupButtonHeart() {
+    let selectedElement = document.getElementById("heart");
+    selectedElement.addEventListener("click", function() {
+        console.log('clicked heart');
+    });
+}
+
+function setupButtonPause() {
+    let selectedElement = document.getElementById("pause");
+    selectedElement.addEventListener("click", function() {
+        console.log('clicked pause');
+    });
 }
